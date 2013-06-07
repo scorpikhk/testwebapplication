60dd6ef819db800ad0cc2b96044ad6aa08dafcf8
Scorpion <Scorpion@Scorpion-PC>
2013-06-07 12:55:08 +0200

    added person entity

---
+++ Testwebapplication/src/main/java/cz/test/web/testwebapplication/person/Person.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.test.web.testwebapplication.person;
+
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+/**
+ *
+ * @author Scorpion
+ */
+
+@Entity
+@Table(name="PERSON")
+public class Person implements Serializable {
+    
+    @GeneratedValue
+    @Id
+    private Integer id;
+    
+    
+    @Column(name="FIRSTNAME")
+    private String firstname;
+    
+    @Column(name="LASTNAME")
+    private String lastname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+    
+    
+    
+    
+    
+}
