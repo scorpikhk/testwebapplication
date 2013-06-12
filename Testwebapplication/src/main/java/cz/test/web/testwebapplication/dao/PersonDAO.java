e6e23377e177c581228d8db8e4d0a039db8b3902
Scorpion <Scorpion@Scorpion-PC>
2013-06-12 21:13:44 +0200

    dao interface

---
+++ Testwebapplication/src/main/java/cz/test/web/testwebapplication/dao/PersonDAO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.test.web.testwebapplication.dao;
+
+import cz.test.web.testwebapplication.entity.Person;
+
+/**
+ *
+ * @author Scorpion
+ */
+public interface PersonDAO {
+    
+    
+    public void addPerson  (Person person);
+    
+    public void editPerson(Person person);
+    
+    public void deletePerson(String id);
+    
+        
+    
+
+}
